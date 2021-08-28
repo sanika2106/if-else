a5e0704db0525353a3a5c85e2f6cadcef82d822c
sanika2106 <89378054+sanika2106@users.noreply.github.com>
2021-08-28 21:53:26 +0530

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,8 @@
+# water=int(input("enter the number"))
+# if water <1:
+#     print("more water needs to be fill")
+# elif water>=10:
+#     print("no need to fill water")
+# else:
+#     print("overflow")
+
